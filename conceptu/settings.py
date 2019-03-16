740be112d359235bd3fba8b63f5f6f3422894dac
Digenaldo Neto <digenaldo.rangel@gmail.com>
2019-03-16 16:02:10 -0300

    alter settings to deploy on heroku

--- conceptu/settings.py
+++ conceptu/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
