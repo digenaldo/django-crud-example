a4c802f073dfb8d36ba5f7144efbd6a12de80eb3
Digenaldo Neto <digenaldo.rangel@gmail.com>
2019-03-16 16:03:51 -0300

    alter path for deploy

--- conceptu/settings.py
+++ conceptu/settings.py
@@ -13,12 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -130,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
