4c47e8c09ec63442c924bd2fbb1dec082826952f
Digenaldo Neto <digenaldo.rangel@gmail.com>
2026-01-30 08:22:15 -0300

    feat: include authentication URLs in main URL configuration

--- conceptu/urls.py
+++ conceptu/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('users.urls')),
     url(r'^', include('products.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
