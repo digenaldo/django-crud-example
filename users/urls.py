3a3e2aa3de58aff028ee51455d0ccadf64f885ee
Digenaldo Neto <digenaldo.rangel@gmail.com>
2026-01-30 08:35:29 -0300

    feat: add health check route

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('healthz/', views.health_check, name='health_check'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
