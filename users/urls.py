9b68dfaf89d9214a8b036ac45069b97dfbab4f95
Digenaldo Neto <digenaldo.rangel@gmail.com>
2026-01-30 08:22:11 -0300

    feat: add authentication URL routes

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
+
