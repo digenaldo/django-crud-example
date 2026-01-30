c4a6947a32e4dce17c37c1ea271cdd35f68d5982
Digenaldo Neto <digenaldo.rangel@gmail.com>
2026-01-30 08:22:12 -0300

    feat: protect product views with LoginRequiredMixin

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView, DetailView 
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from django.urls import reverse_lazy
@@ -8,25 +9,25 @@ from django.urls import reverse_lazy
 from .models import Product
 from .forms import ProductForm
 
-class ProductList(ListView): 
+class ProductList(LoginRequiredMixin, ListView): 
     model = Product
 
-class ProductDetail(DetailView): 
+class ProductDetail(LoginRequiredMixin, DetailView): 
     model = Product
 
-class ProductCreate(SuccessMessageMixin, CreateView): 
+class ProductCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView): 
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('product_list')
     success_message = "Product successfully created!"
 
-class ProductUpdate(SuccessMessageMixin, UpdateView): 
+class ProductUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView): 
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('product_list')
     success_message = "Product successfully updated!"
 
-class ProductDelete(SuccessMessageMixin, DeleteView):
+class ProductDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('product_list')
     success_message = "Product successfully deleted!"
